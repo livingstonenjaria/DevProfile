e9c1004edd9ca481ee7691bc80a9b1c1227bae2f
Livingstone <livingstonenjaria4@gmail.com>
2021-05-17 15:24:57 +0300

    Lesson 04

--- app/src/main/java/com/codestructgroup/devprofile/MainActivity.kt
+++ app/src/main/java/com/codestructgroup/devprofile/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.codestructgroup.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var logo: ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.octocat)
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+        logo = findViewById(R.id.logo)
+        logo.setImageDrawable(rounded)
     }
 }
